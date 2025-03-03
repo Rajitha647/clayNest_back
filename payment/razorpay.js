ea74df98d205ba8c27c7b92bbaa77c364d172b94
Rajitha647/kidsstory <Rajitha647/kidsstory>
2025-03-03 18:54:09 +0530

    19

--- payment/razorpay.js
+++ payment/razorpay.js
@@ -4,5 +4,4 @@ const razorpayInstance = new Razorpay({
   key_id: '123', // Replace with your Razorpay Key ID
   key_secret: 'R123', // Replace with your Razorpay Key Secret
 });
-
-module.exports = { razorpayInstance };
+export default razorpayInstance;
