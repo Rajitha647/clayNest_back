b2191c50b94c796aa3be32378b31f6ecaae4d279
Rajitha647/kidsstory <Rajitha647/kidsstory>
2025-03-03 17:35:05 +0530

    5

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
   });
 
 // Middleware
-app.use(cors({ origin: ["https://your-frontend-domain.com"] })); // Replace with your frontend URL
+app.use(cors({ origin: ["https://clay-nest.onrender.com"] })); // Replace with your frontend URL
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
