30a18685646cdc031dadcbf6447fc555a072cb31
Rajitha647/kidsstory <Rajitha647/kidsstory>
2025-03-03 18:41:32 +0530

    17

--- model/productmodel.js
+++ model/productmodel.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import {usermodel} from './usermodel.js';
+import usermodel from './usermodel';
 const productSchema = new mongoose.Schema({
    userid:{type:mongoose.Schema.Types.ObjectId,ref:usermodel},
     title: {type:String},
